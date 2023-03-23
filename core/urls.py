a2c7bcbf9bda680288001e1fa74cc3960f4383d7
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2023-03-23 13:47:19 +0300

    linked all common pages to the project

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('terms-and-conditions/', views.terms, name='terms'),
+    path('privacy-policy/', views.privacy, name='privacy'),
+    path('faq/', views.faq, name='faq'),
+    path('help/', views.help, name='help'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,18 +3,42 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Home Page',
+        'title': 'Homepage',
     }
-    return render(request, 'base.html', context)
+    return render(request, 'index.html', context)
 
 def about(request):
     context = {
-        'title': 'About Us Page',
+        'title': 'About Us',
     }
-    return render(request, 'base.html', context)
+    return render(request, 'about.html', context)
 
 def contact(request):
     context = {
-        'title': 'Contact Us Page',
+        'title': 'Contact Us',
     }
-    return render(request, 'base.html', context)
+    return render(request, 'contact.html', context)
+
+def terms(request):
+    context = {
+        'title': 'Terms and Conditions',
+    }
+    return render(request, 'terms.html', context)
+
+def privacy(request):
+    context = {
+        'title': 'Privacy Policy',
+    }
+    return render(request, 'privacy.html', context)
+
+def help(request):
+    context = {
+        'title': 'Help Page',
+    }
+    return render(request, 'help.html', context)
+
+def faq(request):
+    context = {
+        'title': 'Frequently Asked Questions',
+    }
+    return render(request, 'faq/index.html', context)
