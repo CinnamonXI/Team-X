078b9d0be55741e3240b54f20b696ceede6e00cb
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2023-03-25 10:51:19 +0300

    updated settings

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Categoty(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category/', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
