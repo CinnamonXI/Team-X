805b92ca2488998079beaa11f7b0ff7049ab9ec1
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2023-03-25 21:37:22 +0300

    udated projects Admin Headings and titles

--- healthlink/urls.py
+++ healthlink/urls.py
@@ -28,3 +28,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+admin.site.site_header = "HealthLink Admin"
+admin.site.site_title = "HealthLink Admin Portal"
