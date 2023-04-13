2da390c9f60427ebc6a8b19fb1a74b26939b1c20
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2023-04-13 18:48:51 +0300

    update settings.py

--- healthlink/settings.py
+++ healthlink/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
+
     'crispy_forms',
     'crispy_bootstrap5',
 
